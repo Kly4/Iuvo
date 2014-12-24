4ed949e85d3e81ae98192693e49ab045ecb6375a
Dan Plyukhin <dplyukhin@gmail.com>
2014-12-23 21:19:20 -0500

    Cleaned getView

--- app/src/main/java/com/iuvo/iuvo/MainActivity.java
+++ app/src/main/java/com/iuvo/iuvo/MainActivity.java
@@ -98,36 +98,30 @@ public class MainActivity extends ActionBarActivity {
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
 
-            ViewHolder viewHolder1;
-            ViewHolder viewHolder2;
-            ViewHolder viewHolder3;
-            ViewHolder viewHolder4;
+            ViewHolder[] views;
+            int[] ids = {
+                R.id.clssname,
+                R.id.clssnmbr,
+                R.id.date,
+                R.id.attd
+            };
+
             if (convertView == null) {
                 convertView = inflater.inflate(R.layout.row_layout, parent, false);
-                viewHolder1 = new ViewHolder();
-                viewHolder2 = new ViewHolder();
-                viewHolder3 = new ViewHolder();
-                viewHolder4 = new ViewHolder();
-                viewHolder1.title = (TextView) convertView.findViewById(R.id.clssname);
-                viewHolder2.title =(TextView) convertView.findViewById(R.id.clssnmbr);
-                viewHolder3.title =(TextView) convertView.findViewById(R.id.date);
-                viewHolder4.title =(TextView) convertView.findViewById(R.id.attd);
-                convertView.setTag(viewHolder1);
-                convertView.setTag(viewHolder2);
-                convertView.setTag(viewHolder3);
-                convertView.setTag(viewHolder4);
+                views = new ViewHolder[4];
+                for (int i = 0; i < views.length; i++) {
+                    views[i].title = (TextView) convertView.findViewById(ids[i]);
+                }
+                convertView.setTag(views);
             } else {
-                viewHolder1 = (ViewHolder) convertView.getTag();
-                viewHolder2 = (ViewHolder) convertView.getTag();
-                viewHolder3 = (ViewHolder) convertView.getTag();
-                viewHolder4 = (ViewHolder) convertView.getTag();
+                views = (ViewHolder[]) convertView.getTag();
             }
 
             Event item = realmResults.get(position);
-            viewHolder1.title.setText(item.getSubject());
-            viewHolder2.title.setText(item.getCourseCode());
-            viewHolder3.title.setText("Dec 21");
-            viewHolder4.title.setText(String.valueOf(item.getNumAttendees()));
+            views[0].title.setText(item.getSubject());
+            views[1].title.setText(item.getCourseCode());
+            views[2].title.setText("Dec 21");
+            views[3].title.setText(String.valueOf(item.getNumAttendees()));
             return convertView;
         }
     }
