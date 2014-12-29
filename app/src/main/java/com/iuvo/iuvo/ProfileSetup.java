898a485a3c7d60e118934782bd318df6f37e06d2
Kly4 <ashablygin@umass.edu>
2014-12-29 17:07:45 -0500

    profile setup

--- app/src/main/java/com/iuvo/iuvo/ProfileSetup.java
+++ app/src/main/java/com/iuvo/iuvo/ProfileSetup.java
@@ -1,16 +1,22 @@
 package com.iuvo.iuvo;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.iuvo.iuvo.schemas.Course;
 
 import io.realm.Realm;
+import io.realm.RealmBaseAdapter;
 import io.realm.RealmResults;
 
 
@@ -32,12 +38,35 @@ public class ProfileSetup extends ActionBarActivity {
         realm = Realm.getInstance(this);
         result = realm.where(Course.class).findAll();
 
+        CustomAdapter courseAdapter = new CustomAdapter(this, result);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, universities);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+        //pickClass.setAdapter(courseAdapter);
 
-        
+    }
 
+    class CustomAdapter extends RealmBaseAdapter<Course> {
+        RealmResults<Course> values;
+        public CustomAdapter(Context ctx, RealmResults<Course> values) {
+            super(ctx, values, false);
+            this.values = values;
+        }
+
+        @Override
+        public View getDropDownView(int position, View convertView, ViewGroup parent){
+            return getView(position, convertView, parent);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            String subject = values.get(position).getSubject();
+            String code = values.get(position).getCourseCode();
+            convertView = inflater.inflate(android.R.layout.simple_dropdown_item_1line, parent, false);
+            TextView txt = (TextView) convertView.findViewById(android.R.id.text1);
+            txt.setText(subject + code);
+            return convertView;
+        }
     }
 
 
@@ -54,12 +83,13 @@ public class ProfileSetup extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_done) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
+
 }
